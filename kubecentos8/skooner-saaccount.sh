6d2e7ef2d34eeb0cac15d26f010249acdf13e678
Hemanth B <hemanth22hemu@gmail.com>
2022-10-05 22:14:47 +0530

    Create skooner-saaccount.sh

---
+++ kubecentos8/skooner-saaccount.sh
@@ -0,0 +1,3 @@
+kubectl create serviceaccount skooner-sa -n kube-system
+kubectl create clusterrolebinding skooner-sa --clusterrole=cluster-admin --serviceaccount=kube-system:skooner-sa
+kubectl get secrets
